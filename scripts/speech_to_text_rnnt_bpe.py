49816fa26fe72d2f100efccb611f64b5f663efd8
hossein khalilian <hse.khalilian08@gmail.com>
2025-07-27 10:30:13 +0000

    add script for finetuning parakeet 0.6b

--- scripts/speech_to_text_rnnt_bpe.py
+++ scripts/speech_to_text_rnnt_bpe.py
@@ -1,45 +1,69 @@
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
-This script can used to fine-tune a speech-to-text model of any instance type when users want to 
-fine-tune an existing model without changing its core architecture but may change the tokenizer. 
-One can mention the pretrained model in two ways: 
-1) `init_from_nemo_model` or 
-2) `init_from_pretrained_model` in the configuration.
+# Preparing the Tokenizer for the dataset
+Use the `process_asr_text_tokenizer.py` script under <NEMO_ROOT>/scripts/tokenizers/ in order to prepare the tokenizer.
 
-****************************************************************************************
-This script is mainly intended for changing the dataset, optim, spec_augment, vocabulary/tokenizer of the model.
-To update the model architecture in conjunction with other modifications, 
-it is advisable to use the primary 'speech_to_text_rnnt/ctc_*.py' script.
-****************************************************************************************
-
-Note: To create a single script for all model types, we currently only support two types of 
-initializations:
-1) `init_from_nemo_model`, and
-2) `init_from_pretrained_model`,
-but not `init_from_ptl_ckpt`.
-
-To train with prior base model tokenizer keep `model.tokenizer.update_tokenizer` as false else
-make it true and provide tokenizer dir along with tokenizer type.
-
-To fine-tune the model, use the following commands:
-
-For initialization from a NEMO model:
 ```sh
-python <NEMO_ROOT>/examples/asr/speech_to_text_finetune.py \
-    init_from_nemo_model=<path_to_nemo_model>
+python <NEMO_ROOT>/scripts/tokenizers/process_asr_text_tokenizer.py \
+        --manifest=<path to train manifest files, seperated by commas>
+        OR
+        --data_file=<path to text data, seperated by commas> \
+        --data_root="<output directory>" \
+        --vocab_size=<number of tokens in vocabulary> \
+        --tokenizer=<"spe" or "wpe"> \
+        --no_lower_case \
+        --spe_type=<"unigram", "bpe", "char" or "word"> \
+        --spe_character_coverage=1.0 \
+        --log
 ```
 
-For initialization from a pretrained model:
+# Training the model
 ```sh
-python <NEMO_ROOT>/examples/asr/speech_to_text_finetune.py \
-    init_from_pretrained_model=<pretrained_model_name>
+python speech_to_text_rnnt_bpe.py \
+    # (Optional: --config-path=<path to dir of configs> --config-name=<name of config without .yaml>) \
+    model.train_ds.manifest_filepath=<path to train manifest> \
+    model.validation_ds.manifest_filepath=<path to val/test manifest> \
+    model.tokenizer.dir=<path to directory of tokenizer (not full path to the vocab file!)> \
+    model.tokenizer.type=<either bpe or wpe> \
+    trainer.devices=-1 \
+    trainer.accelerator="gpu" \
+    trainer.strategy="ddp" \
+    trainer.max_epochs=100 \
+    model.optim.name="adamw" \
+    model.optim.lr=0.001 \
+    model.optim.betas=[0.9,0.999] \
+    model.optim.weight_decay=0.0001 \
+    model.optim.sched.warmup_steps=2000
+    exp_manager.create_wandb_logger=True \
+    exp_manager.wandb_logger_kwargs.name="<Name of experiment>" \
+    exp_manager.wandb_logger_kwargs.project="<Name of project>"
 ```
+
+# Fine-tune a model
+
+For documentation on fine-tuning this model, please visit -
+https://docs.nvidia.com/deeplearning/nemo/user-guide/docs/en/main/asr/configs.html#fine-tuning-configurations
+
 """
 
 import lightning.pytorch as pl
 import torch.nn as nn
 from nemo.collections.asr.models import ASRModel
 from nemo.core.config import hydra_runner
-from nemo.utils import logging, model_utils
+from nemo.utils import logging
 from nemo.utils.exp_manager import exp_manager
 from nemo.utils.trainer_utils import resolve_trainer_cfg
 from omegaconf import OmegaConf
@@ -55,48 +79,6 @@ def enable_bn_se(m):
         m.train()
         for param in m.parameters():
             param.requires_grad_(True)
-
-
-def get_base_model(trainer, cfg):
-    """
-    Returns the base model to be fine-tuned.
-    Currently supports two types of initializations:
-    1) `init_from_nemo_model`, and
-    2) `init_from_pretrained_model`.
-    Args:
-        trainer: PyTorch Lightning Trainer
-        cfg: config
-    Returns:
-        asr_model: ASRModel instance
-    """
-    asr_model = None
-    nemo_model_path = cfg.get("init_from_nemo_model", None)
-    pretrained_name = cfg.get("init_from_pretrained_model", None)
-    if nemo_model_path is not None and pretrained_name is not None:
-        raise ValueError(
-            "Only pass `init_from_nemo_model` or `init_from_pretrained_model` but not both"
-        )
-    elif nemo_model_path is None and pretrained_name is None:
-        raise ValueError(
-            "Both `init_from_nemo_model` and `init_from_pretrained_model cannot be None, should pass atleast one of them"
-        )
-
-    if nemo_model_path is not None:
-        asr_model = ASRModel.restore_from(restore_path=nemo_model_path)
-    elif pretrained_name is not None:
-        asr_model = ASRModel.from_pretrained(model_name=pretrained_name)
-
-    if cfg.model.freeze_encoder:
-        asr_model.encoder.freeze()
-        asr_model.encoder.apply(enable_bn_se)
-        logging.info("Model encoder has been frozen")
-    else:
-        asr_model.encoder.unfreeze()
-        logging.info("Model encoder has been un-frozen")
-
-    asr_model.set_trainer(trainer)
-
-    return asr_model
 
 
 def check_vocabulary(asr_model, cfg):
@@ -193,34 +175,59 @@ def setup_dataloaders(asr_model, cfg):
     return asr_model
 
 
-@hydra_runner(config_path="conf/asr_finetune", config_name="speech_to_text_finetune")
+@hydra_runner(config_path="experimental/contextnet_rnnt", config_name="config_rnnt_bpe")
 def main(cfg):
     logging.info(f"Hydra config: {OmegaConf.to_yaml(cfg)}")
 
     trainer = pl.Trainer(**resolve_trainer_cfg(cfg.trainer))
     exp_manager(trainer, cfg.get("exp_manager", None))
 
-    if hasattr(cfg, "init_from_ptl_ckpt") and cfg.init_from_ptl_ckpt is not None:
-        raise NotImplementedError(
-            "Currently for simplicity of single script for all model types, we only support `init_from_nemo_model` and `init_from_pretrained_model`"
-        )
+    # asr_model = EncDecRNNTBPEModel(cfg=cfg.model, trainer=trainer)
+    # asr_model.maybe_init_from_pretrained_checkpoint(cfg)
 
-    asr_model = get_base_model(trainer, cfg)
+    print(cfg.get("init_from_pretrained_model"))
+    asr_model = ASRModel.from_pretrained(
+        model_name=cfg.get("init_from_pretrained_model")
+    )
 
-    # Check vocabulary type and update if needed
-    asr_model = check_vocabulary(asr_model, cfg)
+    asr_model = update_tokenizer(
+        asr_model, cfg.model.tokenizer.dir, cfg.model.tokenizer.type
+    )
 
-    # Setup Data
-    asr_model = setup_dataloaders(asr_model, cfg)
+    if cfg.model.freeze_encoder:
+        asr_model.encoder.freeze()
+        asr_model.encoder.apply(enable_bn_se)
+        logging.info("Model encoder has been frozen")
+    else:
+        asr_model.encoder.unfreeze()
+        logging.info("Model encoder has been un-frozen")
 
-    # Setup Optimizer
-    asr_model.setup_optimization(cfg.model.optim)
+    # asr_model = check_vocabulary(asr_model, cfg)
+    # asr_model = setup_dataloaders(asr_model, cfg)
+    asr_model.setup_training_data(cfg.model.train_ds)
+    asr_model.setup_validation_data(cfg.model.validation_ds)
 
-    # Setup SpecAug
-    if hasattr(cfg.model, "spec_augment") and cfg.model.spec_augment is not None:
-        asr_model.spec_augment = ASRModel.from_config_dict(cfg.model.spec_augment)
+    # asr_model.setup_optimization(cfg.model.optim)
+    # if hasattr(cfg.model, "spec_augment") and cfg.model.spec_augment is not None:
+    #     asr_model.spec_augment = ASRModel.from_config_dict(cfg.model.spec_augment)
+
+    # asr_model.setup_training_data(cfg.model.train_ds)
+    # asr_model.setup_validation_data(cfg.model.validation_ds)
+    # asr_model.tokenizer_dir = cfg.model.tokenizer.dir
+    # asr_model.change_vocabulary(
+    #     new_tokenizer_dir=cfg.model.tokenizer.dir,
+    #     new_tokenizer_type=cfg.model.tokenizer.type,
+    # )
+    # asr_model.set_trainer(trainer)
 
     trainer.fit(asr_model)
+
+    if (
+        hasattr(cfg.model, "test_ds")
+        and cfg.model.test_ds.manifest_filepath is not None
+    ):
+        if asr_model.prepare_test(trainer):
+            trainer.test(asr_model)
 
 
 if __name__ == "__main__":
